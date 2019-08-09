416847ed9cbafd465aa70f75c6fd5663bec9197e
Mateo <gomezmateo94@gmail.com>
2019-08-08 22:40:45 -0500

    Views

---
+++ src/views/Inicio.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Listado de tareas</h1>
+        <ul>
+            <li v-for="item of tareas" :key="item.id">
+                {{item.id}} - {{item.nombre}}
+                <router-link :to="{name: 'editar', params: {id:item.id}}">
+                    <button>Editar</button>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+import {mapActions, matState, mapState} from 'vuex'
+export default {
+    name: 'Inicio',
+    methods: {
+        ...mapActions(['getTareas'])
+    },
+    created(){
+        this.getTareas()
+    },
+    computed:{
+        ...mapState(['tareas'])
+    }
+}
+</script>
