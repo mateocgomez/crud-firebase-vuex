2867d3c7c9d354518c6098b7092588efd25087d2
Mateo <gomezmateo94@gmail.com>
2019-08-08 22:38:49 -0500

    Tomar objetos de firebase y editarlos desde la vista

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,16 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'inicio',
+      component: () => import(/* webpackChunkName: "about" */ './views/Inicio.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/editar/:id',
+      name: 'editar',
+      component: () => import(/* webpackChunkName: "about" */ './views/Editar.vue')
     }
   ]
 })
